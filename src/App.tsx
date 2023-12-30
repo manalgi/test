97f930025ec99ff6e8e36a95e7b2c6c0b715fb72
manalgi <manalgidev@gmail.com>
2023-12-30 18:25:05 +0100

    feat: Create a new project Cheerfiy Me

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import CheerfyDisplayer from './CheerfyDisplayer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CheerfyDisplayer />
+      </header>
+    </div>
+  );
+}
+
+export default App;
