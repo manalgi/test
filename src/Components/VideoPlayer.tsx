639bf6f994d56c88db57eac6aa28db3d1df79477
manalgi <manalgidev@gmail.com>
2023-12-30 19:59:57 +0100

    feat: Add styling

--- src/Components/VideoPlayer.tsx
+++ src/Components/VideoPlayer.tsx
@@ -1,7 +1,8 @@
-// src/MemeGenerator.tsx
 import React, { useState } from 'react';
 import { Button, Container, TextField } from '@mui/material';
 import { getRandomVideo, Video } from '../Catalog/mediaCatalog';
+import SendIcon from '@mui/icons-material/Send';
+
 
 const VideoPlayer: React.FC = () => {
   const [randomVideo, setRandomVideo] = useState<Video | undefined>(undefined);
@@ -11,28 +12,36 @@ const VideoPlayer: React.FC = () => {
   const handleGenerateVideo = () => {
     const video = getRandomVideo();
     setRandomVideo(video);
+    setTextValue('')
   };
 
   return (
-    <Container>
-
+    <Container style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <div style={{ flex: 1, position: 'relative' }}>
       {randomVideo && (
-        <div>
-         <iframe
-            width="560"
-            height="315"
+          <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
+          <iframe
+             width="100%"
+             height="95%"
             src={`${embadedYoutubeUrl}${randomVideo.youtubeId}?autoplay=1`}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           />
         </div>
       )}
-          <TextField id="outlined-basic" variant="outlined" onChange={(e)=>{
-            setTextValue(e.target.value)
-          }}/>
-          <Button variant="contained" color="primary" onClick={handleGenerateVideo} disabled={textValue === ''}>
-        Generate Random Video
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: 'auto', marginBottom: '8px' }} >
+        <TextField 
+        id="outlined-basic" 
+        variant="outlined" 
+        onChange={(e)=>{ setTextValue(e.target.value) }}  
+        fullWidth
+        value={textValue}/>
+
+        <Button variant="contained" color="primary" onClick={handleGenerateVideo} disabled={textValue === ''} style={{marginLeft:'5px'}}>
+            <SendIcon/>
       </Button>
+      </div>
     </Container>
   );
 };
